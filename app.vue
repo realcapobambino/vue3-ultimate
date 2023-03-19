311da742879fdecc2facf82e49619b4a8d70b501
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-03-19 12:26:22 +0300

    more changes

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div>
-		<h1>BossBaby's Baby Name Generator</h1>
-		<p>Choose your options and click the "Find Names button below"</p>
-		<div class="options-container">
-			<div class="option-container"></div>
-		</div>
-	</div>
-</template>
