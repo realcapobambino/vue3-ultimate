7b2612dc4e9677301895b1e8078b3a9f98c4797e
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-03-20 12:07:25 +0300

    done events

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ const app = Vue.createApp(
             return {
                 showCounter: false,
                 count: 0,
+                x: 0,
+                y: 0,
+                people: [
+                    { name: 'Max', age: 30, color: 'red' },
+                    { name: 'Manu', age: 29, color: 'blue' },
+                    { name: 'Julie', age: 28, color: 'green' }
+                ]
             }
 
         },
         methods: {
             toggleShowDiv() {
                 this.showCounter = !this.showCounter;
+            },
+            handleEvent(event, data) {
+                console.log(event, event.type)
+                if (data) {
+                    console.log(data)
+                }
+            },
+            handleMousemove(event) {
+                this.x = event.offsetX;
+                this.y = event.offsetY;
             }
         }
     }
