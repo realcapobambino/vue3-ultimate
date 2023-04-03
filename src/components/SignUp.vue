dbbfe92e04ebbb59b65846651b1851dde5b5af14
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-04-03 20:10:13 +0300

    attempt remove challenge #1

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -15,7 +15,7 @@
         <label>Skills:</label>
         <!-- keyboard event. modified by alt key -->
         <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
-        <div v-for="skill in skills" :key="skill" class="pill">
+        <div v-for="skill in skills" :key="skill" class="pill" @click="removeTempSkill">
             <p>{{ skill }}</p>
         </div>
 
@@ -58,7 +58,7 @@ export default {
             terms: false,
             // names: []
             tempSkill: '',
-            skills: []
+            skills: ['html', 'css']
         }
     },
     methods: {
@@ -71,6 +71,9 @@ export default {
 
                 this.tempSkill = ''
             }
+        },
+        removeTempSkill() {
+            console.log('clicked:', this.tempSkill)
         }
     }
 }
@@ -121,5 +124,18 @@ input[type="checkbox"] {
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+}
+
+.pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
 }
 </style>
