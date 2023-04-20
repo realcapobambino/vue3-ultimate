06bd9fa6105820bbc7d9e857666496dd275e4074
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-04-20 18:35:37 +0300

    remove temp data

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -11,11 +11,7 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "Vue Developer", id: 1, details: "Lorem" },
-        { title: "React Developer", id: 2, details: "Lorem" },
-        { title: "Angular Developer", id: 3, details: "Lorem" },
-      ],
+      jobs: [],
     };
   },
 };
