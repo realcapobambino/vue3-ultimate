dd84171de53480c893bb6c084c142eba8d1065fe
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-05-01 12:16:34 +0300

    finished composition api basics

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     Home
-
+    <p>My name is {{ name }}. I am {{ age }} years old</p>
+    <button @click="handleClick">Click Me</button>
   </div>
 </template>
 
@@ -13,12 +14,27 @@ export default {
   name: 'HomeView',
   setup() {
     console.log('setup')
+
+    let name = 'Karen'
+    let age = 26
+
+
+    const handleClick = () => {
+      console.log('clicked')
+    }
+
+    return {
+      name,
+      age,
+      handleClick
+    }
+
   },
-  created() {
-    console.log('created hook')
-  },
-  mounted() {
-    console.log('mounted hook')
-  },
+  // created() {
+  //   console.log('created hook')
+  // },
+  // mounted() {
+  //   console.log('mounted hook')
+  // },
 }
 </script>
