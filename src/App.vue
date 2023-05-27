dd2973a6511d190e5a0ae0314fe7dbd75db472b9
realcapobambino <charliealienchamp@gmail.com>
2023-05-27 04:10:25 +0300

    rename

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <router-view />
+	<router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  /* -moz-osx-font-smoothing: grayscale; */
-  text-align: center;
-  color: #2c3e50;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		/* -moz-osx-font-smoothing: grayscale; */
+		text-align: center;
+		color: #2c3e50;
+	}
+
+	body {
+		background-color: grey;
+	}
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="home">
+		Home
+		<p ref="p">My name is {{ name }}. I am {{ age }} years old</p>
+		<button @click="handleClick">Click Me</button>
+	</div>
+</template>
+
+<script>
+	import { ref } from 'vue'
+
+	export default {
+		name: 'Home',
+		setup() {
+			// const p = ref(null)
+
+			const name = ref('karen')
+			const age = ref(30)
+
+			// let name = 'Karen'
+			// let age = 26
+
+			const handleClick = () => {
+				// console.log(p, p.value)
+				// p.value.classList.add('test')
+				// p.value.textContent = 'Hello World'
+				// name = 'kate'
+
+				name.value = 'kate'
+				age.value = 26
+			}
+
+			return {
+				name,
+				age,
+				handleClick,
+			}
+		},
+		// created() {
+		//   console.log('created hook')
+		// },
+		// mounted() {
+		//   console.log('mounted hook')
+		// },
+	}
+</script>
+
+<style>
+	.home {
+		background-color: green;
+	}
+</style>
