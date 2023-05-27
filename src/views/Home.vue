b3cc716bcb8758ce461c10733856da59e9e92fab
realcapobambino <charliealienchamp@gmail.com>
2023-05-27 04:37:09 +0300

    reactive vs refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,23 @@
 <template>
-	<div class="home">
-		Home
+	<div class="card home1">
+		<h1>Home</h1>
 		<p ref="p">My name is {{ name }}. I am {{ age }} years old</p>
 		<button @click="handleClick">Click Me</button>
+		<button @click="age++">Add 1</button>
+		<input type="text" v-model="name" />
+	</div>
+	<div class="card home2">
+		<h1>using refs</h1>
+		<p>{{ humanOne.name }} - {{ humanOne.age }}</p>
+		<button @click="updateHumanOne">Update Human One</button>
+		<h1>using reactive</h1>
+		<p>{{ humanTwo.name }} - {{ humanTwo.age }}</p>
+		<button @click="updateHumanTwo">Update Human Two</button>
 	</div>
 </template>
 
 <script>
-	import { ref } from 'vue'
+	import { ref, reactive } from 'vue'
 
 	export default {
 		name: 'Home',
@@ -30,10 +40,25 @@
 				age.value = 26
 			}
 
+			const humanOne = ref({ name: 'Atom', age: 25 })
+			const humanTwo = reactive({ name: 'Ele', age: 24 })
+
+			const updateHumanOne = () => {
+				humanOne.value.age = 30
+			}
+
+			const updateHumanTwo = () => {
+				humanTwo.age = 28
+			}
+
 			return {
 				name,
 				age,
 				handleClick,
+				humanOne,
+				updateHumanOne,
+				humanTwo,
+				updateHumanTwo,
 			}
 		},
 		// created() {
@@ -46,7 +71,19 @@
 </script>
 
 <style>
-	.home {
+	.card {
+		max-width: 75%;
+		padding: 10px;
+		margin: 25px auto;
+		margin-bottom: 20px;
+		border-radius: 30px;
+		color: white;
+	}
+	.home1 {
 		background-color: green;
+	}
+	.home2 {
+		background-color: aqua;
+		color: black;
 	}
 </style>
